0811db774dbaebf4dadaf74cb2ee6faa18fe40c8
Kunal Limaye <kunall@google.com>
2021-06-12 13:09:09 +1000

    Added secret to k8s manifests

--- infra/2-deploy-platform/scripts/get-bank-of-anthos-kubernetes-manifests.sh
+++ infra/2-deploy-platform/scripts/get-bank-of-anthos-kubernetes-manifests.sh
@@ -4,7 +4,10 @@ rm -fr ${KUBE_MANIFEST_DIR}
 mkdir -p ${KUBE_MANIFEST_DIR}
 
 cd ${KUBE_MANIFEST_DIR}
-manifest_list=("accounts-db" "balancer-reader" "config" "contacts" "frontend" "ledger-db" "ledger-writer" "loadgenerator" "transaction-history" "userservice")
+manifest_list=("accounts-db" "balance-reader" "config" "contacts" "frontend" "ledger-db" "ledger-writer" "loadgenerator" "transaction-history" "userservice")
 for each_manifest in ${manifest_list[@]}; do
     curl -LO https://github.com/GoogleCloudPlatform/bank-of-anthos/raw/master/kubernetes-manifests/${each_manifest}.yaml
 done
+mkdir -p secret
+cd secret
+curl -LO https://github.com/GoogleCloudPlatform/bank-of-anthos/raw/master/extras/jwt/jwt-secret.yaml
