20163b8ab53ebfd3d16be59b0ebedc945c470d98
Kunal Limaye <kunall@google.com>
2021-06-17 15:52:27 +1000

    Removing files that are not used anymore

--- infra/1-compute-vms/scripts/accounts-db-config.sh
+++
@@ -1,6 +0,0 @@
-    -e POSTGRES_DB=accounts-db \
-    -e POSTGRES_USER=accounts-admin \
-    -e POSTGRES_PASSWORD=accounts-pwd \
-    -e ACCOUNTS_DB_URI=postgresql://accounts-admin:accounts-pwd@accounts-db:5432/accounts-db \
-
-/snap/bin/yq e '.spec.template.spec.containers[0].env[] | select(.name == "PORT") | .value = "8081"' -i contacts.yaml
--- infra/1-compute-vms/scripts/setup-bank-of-anthos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WORK_DIR=../.target
-rm -fr ${WORK_DIR}
-mkdir ${WORK_DIR}; cd ${WORK_DIR}
-# git clone the repo
-git clone https://github.com/finiteloopme/bank-of-anthos
